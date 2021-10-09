29882527f15763029af0ae8b9e955b4ccad374d0
M.RAY <a.mray47@yahoo.fr>
2021-10-09 21:03:38 +0800

    Linked the style.css to html

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req,res) => {
 })
 
 //loading the css file
-app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 app.use("*/css", express.static(__dirname + "public/css"));
 app.use("*/images", express.static("public/images"));
 
